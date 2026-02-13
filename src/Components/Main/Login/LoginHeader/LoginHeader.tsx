e268b041643709b4b96465309ba6a3e0e59295c2
mohammadalomer1991 <mohammadalomer1991@gmail.com>
2026-02-13 14:42:23 +0900

    intial commit

---
+++ src/Components/Main/Login/LoginHeader/LoginHeader.tsx
@@ -0,0 +1,17 @@
+import './LoginHeader.css'
+const LoginHeader=()=>{
+    return <div className="header">
+        <svg>
+        <circle 
+            cx="12" 
+            cy="12" 
+            r="10" 
+            stroke="gray" 
+            stroke-width="4" 
+            fill="gray" 
+            />
+        </svg>
+    </div>
+}
+
+export  default LoginHeader
